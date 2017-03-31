e09a0d94fb70bf4bb11565010a69b7bb10265c29
Amanda Birmingham <abirmingham@ucsd.edu>
2017-03-31 13:06:12 -0700

    reformatted lists to add spaces between items, per PEP

--- tests/test_string_utils.py
+++ tests/test_string_utils.py
@@ -7,8 +7,8 @@ import ccbb_pyutils.string_utils as ns_test
 class TestFunctions(unittest.TestCase):
     def test_split_delimited_string_to_list_defaults(self):
         real_output = ns_test.split_delimited_string_to_list("1,3,5,9,blue")
-        self.assertEqual(["1","3","5","9","blue"], real_output)
+        self.assertEqual(["1", '3', "5", "9", "blue"], real_output)
 
     def test_split_delimited_string_to_list_no_defaults(self):
         real_output = ns_test.split_delimited_string_to_list("1;3;5;9", int, ";")
-        self.assertEqual([1,3,5,9], real_output)
+        self.assertEqual([1, 3, 5, 9], real_output)
